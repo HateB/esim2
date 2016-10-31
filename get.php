e3e51037bf087d78a020f1a0d9e016aa4088b01f
Unknown <paasimaa.janne@hotmail.com>
2016-10-31 10:33:43 +0200

    Post.php typo korjattu ja luotu get.php

--- get.php
+++ get.php
@@ -1,18 +1,18 @@
-<h1>Testataan post-metodia</h1>
-<form method="post" action="post.php">
+<h1>Testataan get-metodia</h1>
+<form method="get" action="get.php">
 <label>Etunimi	</label>
 <input type="text" name="en">
 <br>
 <label>Sukunimi	</label>
-<input type="text" name="sn">
+<input type="text" name="sn" required="">
 <br>
 <input type="submit" name="nappi" value="Lähetä">
 </form>
 
 <?php
-if(isset($_POST['nappi'])){
-	$enimi=$_POST['en'];
-	$snimi=$_POST['sn'];
+if(isset($_GET['nappi'])){
+	$enimi=$_GET['en'];
+	$snimi=$_GET['sn'];
 	echo "Terve $enimi $snimi";
 }
 ?>
